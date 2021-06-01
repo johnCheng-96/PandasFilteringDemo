a04e6c472a4d59a8818a6dd042e5895b86580c71
johnyhcheng <john.cheng@fdmgroup.com>
2021-06-02 00:17:56 +0800

    Countries with 5M or more total cases ( > operator and query)

--- countriesWith5MOrMoreCases.py
+++ countriesWith5MOrMoreCases.py
@@ -1,5 +1,4 @@
-# TODO: Countries with 5M or more total cases ( > operator)
-
+"""Countries with 5M or more total cases ( > operator and query)"""
 
 import pandas as pd
 import numpy as np
@@ -8,4 +7,16 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     ''' read data from csv '''
-    covidVaccineDf = pd.read_csv("resources/owid-covid-data.csv")
+    covidDf = pd.read_csv("resources/owid-covid-data.csv")
+
+    ''' remove "World" and Continent data first '''
+
+    countriesDf = covidDf[covidDf.continent.notna()]
+
+    print(countriesDf.to_markdown)
+
+    ''' get the row for each country with its max date '''
+
+    countriesDfUpToDate = countriesDf.groupby(by=['location']).apply(lambda x: x[x['date'] == x['date'].max()])
+
+    print(countriesDfUpToDate.query('total_cases > 5_000_000')[['location', 'total_cases']])
