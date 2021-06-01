74e2d2ee42a9204f62e366299123d70f26566c02
johnyhcheng <john.cheng@fdmgroup.com>
2021-06-01 21:31:39 +0800

    which country has the highest accumulated covid cases?

--- showAllLocations.py
+++ showAllLocations.py
@@ -7,5 +7,4 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     ''' read data from csv '''
     covidDf = pd.read_csv("resources/owid-covid-data.csv")
-
     print(covidDf['location'].unique())
--- whichCountryHasTheHighestAccumulatedCovidCases.py
+++ whichCountryHasTheHighestAccumulatedCovidCases.py
@@ -1,5 +1,3 @@
-# TODO: which country has the highest accumulated covid cases?
-
 '''
 which country has the highest accumulated covid cases?
 '''
@@ -11,14 +9,14 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     covidDf = pd.read_csv("resources/owid-covid-data.csv")
 
-    ''' need to get rid of the world data (global total cases) '''
-    totalCasesAtLocation = covidDf[covidDf.location != 'World'][['date', 'location', 'total_cases']]
+    ''' need to get rid of the world and continent data '''
+    ''' by removing rows with null value in 'continent' column '''
+    totalCasesAtLocation = covidDf[covidDf.continent.notna()][['date', 'location', 'total_cases']]
 
-    print(totalCasesAtLocation)
 
     ''' Here just need to find the max number of total cases of all data '''
     ''' then we know the location with the most accumulated Covid cases '''
-    print(totalCasesAtLocation.total_cases.max())
+    maxNumOfCases = totalCasesAtLocation.total_cases.max()
 
-    print(totalCasesAtLocation[totalCasesAtLocation.total_cases == totalCasesAtLocation.total_cases.max()])
+    print(totalCasesAtLocation[totalCasesAtLocation.total_cases == maxNumOfCases])
 
